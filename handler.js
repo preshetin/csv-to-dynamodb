c833e8cade2f86c79b086dc914bd9259e5c7c766
Petr Reshetin <preshetin@gmail.com>
2020-01-06 16:38:46 +0300

    Apha version ready

--- handler.js
+++ handler.js
@@ -1,4 +1,32 @@
+import fetch from 'node-fetch';
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+export const save = (event, context, callback) => {
+  console.log('save function invoking...');
+  fetch(event.image_url)
+    .then((response) => {
+      if (response.ok) {
+        return response;
+      }
+      return Promise.reject(new Error(
+            `Failed to fetch ${response.url}: ${response.status} ${response.statusText}`));
+    })
+    .then(response => response.buffer())
+    .then(buffer => (
+      s3.putObject({
+        Bucket: process.env.BUCKET,
+        Key: event.key,
+        Body: buffer,
+      }).promise()
+    ))
+    .then(v => callback(null, v), callback);
+};
+
+
 export const hello = async (event, context) => {
+  console.log('doing console.log....');
   return {
     statusCode: 200,
     body: JSON.stringify({
---
+++ import.js
@@ -0,0 +1,66 @@
+import AWS from 'aws-sdk';
+import parse from 'csv-parser';
+
+const tableHeaders = [
+  'item0', 'item1', 'item2',
+  'item3', 'item4', 'item5',
+  'item6', 'item7', 'item8',
+  'item9', 'item10', 'item11',
+  'item12', 'item13', 'item14',
+  'item15', 'item16', 'item17',
+  'item18', 'licenceId'
+];
+
+const s3 = new AWS.S3();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export const main = (event, context, callback) => {
+
+  const data_arr = [];
+  const bucket = event.Records[0].s3.bucket.name;
+  const objectkey = event.Records[0].s3.object.key;
+  const params = { Bucket: bucket, Key: objectkey };
+
+  console.log('importing file: ', objectkey);
+
+  var file = s3.getObject(params).createReadStream();
+
+  file.pipe(parse(tableHeaders))
+    .on('data', function (data) {
+      const parsed_data = JSON.parse(JSON.stringify(data));
+      data_arr.push(parsed_data);
+    })
+    .on('end', function () {
+      update();
+    });
+
+  function update() {
+    try {
+      data_arr.forEach(item => {
+        const params = getParams(item);
+        dynamoDb.update(params, (err, data) => {
+          if (err) console.error(err);
+        });
+      });
+      callback(null, 'all good');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function getParams(item) {
+    return {
+      TableName: process.env.tableName,
+      Key: {
+        licenceId: item.licenceId,
+        sk: item.item0
+      },
+      UpdateExpression: "SET item1 = :item1, item2 = :item2",
+      ExpressionAttributeValues: {
+        ":item1": item.item1,
+        ":item2": item.item2
+      },
+      ReturnValues: "ALL_NEW"
+    };
+  }
+};
